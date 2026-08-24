1bd938441f1200b82e44ce8bd586039fde2d61c1
Muhammad Bilal Khan <muhammadbilalkhan1120@gmail.com>
2026-08-24 11:42:45 +0500

    fix: update Calvin Klein image import path and remove old image file

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -4,7 +4,7 @@ import versace from "../../assets/versace.png";
 import zara from "../../assets/zara.png";
 import gucci from "../../assets/gucci.png";
 import prada from "../../assets/prada.png";
-import calvinKlein from "../../assets/calvin Klien.png";
+import calvinKlein from "../../assets/calvin-klien.png";
 
 function Brands() {
   return (
@@ -12,13 +12,13 @@ function Brands() {
 
       <img src={versace} alt="Versace" />
 
-       <img src={zara} alt="Zara" />
+      <img src={zara} alt="Zara" />
 
       <img src={gucci} alt="Gucci" />
 
       <img src={prada} alt="Prada" />
 
-      <img src={calvinKlein} alt="Calvin Klein" /> 
+      <img src={calvinKlein} alt="Calvin Klein" />
 
     </section>
   );
